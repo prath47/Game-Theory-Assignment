4b9b4c9c7572494587cf504c5c2f7ff0f18f7186
Prathmesh <148199267+prath47@users.noreply.github.com>
2024-10-17 04:15:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ mongoose
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://game-theory-assignment-frontend.onrender.com"],
     credentials: true,
   })
 );
